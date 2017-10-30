92e118e4daa314b2145ec221f1f15f6ad4c36818
Hossam Hassan <hoss_9063@hotmail.com>
2017-10-31 00:14:59 +0200

    Handeled cd cases.

--- commands.c
+++ commands.c
@@ -3,22 +3,29 @@
 //
 
 #include <stdio.h>
+#include <pwd.h>
+#include <unistd.h>
 #include <zconf.h>
+#include <strings.h>
 #include "tokenizer.h"
+
 
 /*
   Builtin function implementations.
 */
 
 int __cd(Tokens tokens) {
+    // get the home directory path using the user_id
+    uid_t uid = getuid();
+    struct passwd *pw = getpwuid(uid);
 
     // cd at background is neglected
     if (tokens.background) return 1;
 
     char **args = tokens.args;
 
-    if (args[1] == NULL) {
-        fprintf(stderr, "hs-shell: expected argument to \"cd\"\n");
+    if (args[1] == NULL || strcmp(args[1],"~") == 0) {
+        chdir(pw->pw_dir);
     } else {
         if (chdir(args[1]) != 0) {
             perror("hs-shell");
