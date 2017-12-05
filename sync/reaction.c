566830a5b97d211750e6bb6a51eeff59b0657778
Ahmed Lotfey <a7med.lotfey@gmail.com>
2017-12-05 03:49:50 +0200

    Done.

--- sync/reaction.c
+++ sync/reaction.c
@@ -7,21 +7,35 @@
 void make_water();
 
 
-void reaction_init(struct reaction *reaction)
-{
-	// FILL ME IN
+void reaction_init(struct reaction *reaction) {
+    // FILL ME IN
+    reaction->H_cnt = 0;
+    pthread_cond_init(&reaction->H, NULL);
+    pthread_cond_init(&reaction->reaction_done, NULL);
+    pthread_mutex_init(&reaction->lock, NULL);
 }
 
 
-void reaction_h(struct reaction *reaction)
-{
-	// FILL ME IN
+void reaction_h(struct reaction *reaction) {
+    // FILL ME IN
+    pthread_mutex_lock(&reaction->lock);
+    reaction->H_cnt++;
+    pthread_cond_signal(&reaction->H);
+    pthread_cond_wait(&reaction->reaction_done, &reaction->lock);
+    pthread_mutex_unlock(&reaction->lock);
 }
 
 
-void reaction_o(struct reaction *reaction)
-{
-	// FILL ME IN
+void reaction_o(struct reaction *reaction) {
+    // FILL ME IN
+    pthread_mutex_lock(&reaction->lock);
+    while (reaction->H_cnt < 2)
+        pthread_cond_wait(&reaction->H, &reaction->lock);
+
+    make_water();
+
+    reaction->H_cnt -= 2;
+    pthread_cond_signal(&reaction->reaction_done);
+    pthread_cond_signal(&reaction->reaction_done);
+    pthread_mutex_unlock(&reaction->lock);
 }
-
-
--- sync/reaction.h
+++ sync/reaction.h
@@ -2,7 +2,11 @@
 #include <pthread.h>
 
 struct reaction {
-	// FILL ME IN
+    // FILL ME IN
+    int H_cnt;
+    pthread_cond_t H;
+    pthread_cond_t reaction_done;
+    pthread_mutex_t lock;
 };
 
 void reaction_init(struct reaction *reaction);
